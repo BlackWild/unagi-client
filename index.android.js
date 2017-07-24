65a6f477c099a50b524981365334849ac9758562
Siavash <siavash.davani@gmail.com>
2017-07-25 02:19:54 +0430

    clean code using imported functions

--- index.android.js
+++ index.android.js
@@ -9,7 +9,6 @@ import {
   StatusBar,
   Button,
   Image,
-  AsyncStorage
 } from 'react-native';
 import ActionButton from 'react-native-action-button';
 import uniqueId from 'react-native-unique-id';
@@ -17,84 +16,28 @@ import { StackNavigator } from 'react-navigation';
 
 import {Post} from './components/Post';
 import {styles} from './styles/Styles';
-
-const tempState = {
-      Posts: [
-        {
-          userID: '454364343664',
-          location: { x: 2312.213, y: 4234.21 },
-          content: 'salama aleyka',
-        },
-        {
-          userID: 'sd12e1ssdew',
-          location: { x: 23344542.213, y: 424534.21 },
-          content: 'salama aleyka sdasd',
-        },
-        {
-          userID: '4523rwvq3fvb3',
-          location: { x: 3.213, y: 4234.21 },
-          content: 'salama12323131d aleyka',
-        },
-        {
-          userID: '454364343664',
-          location: { x: 2312.213, y: 4234.21 },
-          content: 'salama aleyka',
-        }
-
-      ]
-    };
+import {setIDState, setLocationState, setPostState} from './functions/StateSetters';
 
 
 class unagiyooooo extends Component {
   constructor(props) {
     super(props);
-    this.setState.bind(this);
-    this.state = tempState;
+    // this.setState.bind(this);
+    // getIDState.bind(this);
   }
-
+  
   componentWillMount() {
-    
-    //////////////////////
-
-    // AsyncStorage.removeItem("userID").then((res) => {
-    //   console.log("\n item deleted");
-    // })
-    AsyncStorage.getItem("userID").then((res) => {
-      console.log("id was:  " + res);
-      if(res) {
-        console.log("is not first run");
-        this.setState(() => {
-          return {userID: res};
-        });
-      } else {
-        uniqueId()
-          .then(id => {
-            console.log("id is : " + id);
-            AsyncStorage.setItem("userID", id).then((res) => {
-              console.log("id saved!! ", res);
-            });
-            this.setState(() => {
-              return {userID: id};
-            });
-            // fetch({
-            //   url: 'https://local..../register/felan?userID=' + id,
-            //   method: 'GET'
-            // }).then((res) => {
-            //     console.log("id sent to server");
-            //     console.log(res);
-            //   })
-            //   .catch((error) => {
-            //     console.error(error);
-            //   });
-          })
-          .catch(error => console.error(error));
-      }
-    });
-
+    setIDState(this);
+    setLocationState(this);
+    if( this.state.userID && this.state.location ) {
+      setPostState("id", "loc" , this);
+    }
+  
   }
 
   render() {
-    if (!this.state.userID || !this.state.Posts) {
+    if (!this.state.Posts) {
+      console.log("not ready");
       return null;
     } else {
       return (
