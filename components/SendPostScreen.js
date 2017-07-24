bc4345ce304f3c07ee5395410a66a1d796c553c6
Siavash <siavash.davani@gmail.com>
2017-07-25 02:35:35 +0430

    styles moved to js file and imported

--- components/SendPostScreen.js
+++ components/SendPostScreen.js
@@ -15,6 +15,8 @@ import {
   StatusBar
 } from 'react-native';
 
+import {styles} from '../styles/SendPostScreenStyles';
+
 export default class test extends Component {
   
   constructor(props) {
@@ -58,49 +60,5 @@ export default class test extends Component {
     );
   }
 }
-
-
-const styles = StyleSheet.create({
-  pic:{
-    width: 30, height: 30,
-    margin:10
-  },
-  bar:{
-    padding: 10,
-    flexDirection: 'row',
-    backgroundColor: '#8BC34A',
-  },
-  container: {
-    flex:1,
-    justifyContent: 'center',
-    alignItems: 'center', 
-    backgroundColor: '#F1F1F1',
-
-  },
-  post: {
-    fontSize: 20,
-    color:'#F1F1F1',
-    textAlign: 'center',
-    justifyContent:'center',
-    margin: 10,
-  },
-  charRemain: {
-    fontSize: 20,
-    color:'#F1F1F1',
-    textAlign: 'center',
-    justifyContent:'center',
-    margin: 10,
-  },
-  textarea: {
-    flex:1,
-    width:350,
-    color: '#000000',
-    backgroundColor: '#F1F1F1',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    textAlign: 'auto',
-    textAlignVertical: 'top'    
-  }
-});
 
 AppRegistry.registerComponent('test', () => test);
