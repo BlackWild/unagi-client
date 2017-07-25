982aaa414e40319cf673b52d6424ecb062632f1f
Siavash <siavash.davani@gmail.com>
2017-07-25 13:52:58 +0430

    layout and logic optimization

--- components/SendPostScreen.js
+++ components/SendPostScreen.js
@@ -25,33 +25,36 @@ export class SendPostScreen extends Component {
   }
 
   render() {
-    this.state.textLenght=this.state.text.split('').length;
+
+    let textLenght = this.state.text.split('').length;
     var overChar = {};
-    if(this.state.textLenght>160){
+    if(textLenght>160){
       overChar = {
         color: '#ff0000' 
       } 
     }
     
     return (
-      <View style={styles.container}>
-        {/* <View style={styles.bar}>
-          <View style={{flex: 1, flexDirection: 'row'}}>
-          <Text style={styles.post}>
-            افزودن پست
-          </Text>
+      <View style={{ flex: 1 }}>
+        <View style={styles.container}>
+          {/* <View style={styles.bar}>
+            <View style={{flex: 1, flexDirection: 'row'}}>
+            <Text style={styles.post}>
+              افزودن پست
+            </Text>
+            </View>
+            <Text style={[styles.charRemain, overChar]}>
+              {160-this.state.textLenght}
+            </Text>
+            <Image source={require('./img/send1.png')} style={styles.pic}/>   
+          </View> */}
+          <View style={{flex:1}}>
+          <TextInput style={styles.textarea} underlineColorAndroid="transparent" multiline={true} 
+            placeholder="بنویسید" 
+            onChangeText={(text) => this.setState({text})}
+          />
+          
           </View>
-          <Text style={[styles.charRemain, overChar]}>
-            {160-this.state.textLenght}
-          </Text>
-           <Image source={require('./img/send1.png')} style={styles.pic}/>   
-        </View> */}
-        <View style={{flex:1}}>
-        <TextInput style={styles.textarea} underlineColorAndroid="transparent" multiline={true} 
-          placeholder="بنویسید" 
-          onChangeText={(text) => this.setState({text})}
-        />
-        
         </View>
       </View>
       
