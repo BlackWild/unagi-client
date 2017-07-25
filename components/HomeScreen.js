cbd5c122c892f65cf1f8f02e5432da9367026989
Siavash <siavash.davani@gmail.com>
2017-07-25 12:38:26 +0430

    Putting HomeScreen in Seperate js file

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,4 +1,68 @@
 
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  StatusBar,
+  Button,
+  Image,
+} from 'react-native';
+import ActionButton from 'react-native-action-button';
 
-///// Will get everything from index.android.js file after setting up navigation in index.android.js
+import { StackNavigator } from 'react-navigation';
 
+import async from 'async';
+
+import {Post} from '../components/Post';
+import {styles} from '../styles/HomeScreenStyles';
+import {setIDState, setLocationState, setPostState} from '../functions/StateSetters';
+
+
+export class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+    // getIDState.bind(this);
+  }
+  
+  componentWillMount() {
+    
+    async.parallel([
+      (callback) => {
+        setIDState(this).then( () => callback()).catch( () => {} );
+      },
+      (callback) => {
+        setLocationState(this).then( () => callback()).catch( () => {} );
+      }
+    ], (err) => {
+      setPostState("id", "loc" , this).then( () => console.log("good")).catch( () => {} );
+    });
+  
+  }
+
+  render() {
+    if (!this.state || !this.state.Posts) {
+      console.log("not ready");
+      return null;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <StatusBar backgroundColor='#689F38' barStyle="light-content" />
+          <View style={styles.headerBox} >
+            <Text style={styles.logo}>اوناگی</Text>
+          </View>
+          <View style={styles.container}>
+            <ScrollView>
+              {this.state.Posts.map((post) => (<Post key={this.state.Posts.indexOf(post)} content={post.content} />))}
+            </ScrollView>
+            <ActionButton degrees={0} offsetX={10} offsetY={20} buttonColor="rgba(231,76,60,1)">
+              <Text style={styles.actionButtonIcon}>+</Text>
+            </ActionButton>
+          </View>
+        </View>
+      );
+    }
+  }
+
+}
--- index.android.js
+++ index.android.js
@@ -1,76 +1,13 @@
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  StatusBar,
-  Button,
-  Image,
-} from 'react-native';
-import ActionButton from 'react-native-action-button';
-import uniqueId from 'react-native-unique-id';
+
+import { AppRegistry } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-import async from 'async';
-
-import {Post} from './components/Post';
-import {styles} from './styles/HomeScreenStyles';
-import {setIDState, setLocationState, setPostState} from './functions/StateSetters';
-
-
-class unagiyooooo extends Component {
-  constructor(props) {
-    super(props);
-    // getIDState.bind(this);
-  }
-  
-  componentWillMount() {
-    
-    async.parallel([
-      (callback) => {
-        setIDState(this).then( () => callback()).catch( () => {} );
-      },
-      (callback) => {
-        setLocationState(this).then( () => callback()).catch( () => {} );
-      }
-    ], (err) => {
-      setPostState("id", "loc" , this).then( () => console.log("good")).catch( () => {} );
-    });
-  
-  }
-
-  render() {
-    if (!this.state || !this.state.Posts) {
-      console.log("not ready");
-      return null;
-    } else {
-      return (
-        <View style={{ flex: 1 }}>
-          <StatusBar backgroundColor='#689F38' barStyle="light-content" />
-          <View style={styles.headerBox} >
-            <Text style={styles.logo}>اوناگی</Text>
-          </View>
-          <View style={styles.container}>
-            <ScrollView>
-              {this.state.Posts.map((post) => (<Post key={this.state.Posts.indexOf(post)} content={post.content} />))}
-            </ScrollView>
-            <ActionButton degrees={0} offsetX={10} offsetY={20} buttonColor="rgba(231,76,60,1)">
-              <Text style={styles.actionButtonIcon}>+</Text>
-            </ActionButton>
-          </View>
-        </View>
-      );
-    }
-  }
-
-}
+import { HomeScreen } from './components/HomeScreen';
 
 // const App = StackNavigator({
 //   Home: { screen: unagiyooooo },
 //   Profile: { screen: unagiyooooo },
 // })
 
-AppRegistry.registerComponent('unagiyooooo', () => unagiyooooo);
+AppRegistry.registerComponent('unagiyooooo', () => HomeScreen);
