0ea25b3abf1fbd5a58c29e870a1a451966752198
AMDaban <daban.am56@gmail.com>
2017-08-01 14:48:02 +0430

    Add Store

--- index.android.js
+++ index.android.js
@@ -4,9 +4,24 @@ import { StackNavigator } from 'react-navigation';
 import {SendPostScreen} from './components/SendPostScreen';
 import {TabScreen} from './components/TabScreen';
 
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+import React, { Component } from 'react';
+import {testReducer} from './reducers/Reducers'
+
 const App = StackNavigator({
   Home: { screen: TabScreen },
   SendPostScreen: { screen: SendPostScreen },
-})
+});
 
-AppRegistry.registerComponent('Unagi', () => App);
+class Unagi extends Component {
+    render() {
+        return (
+            <Provider store={createStore(testReducer)}>
+              <App/>
+            </Provider>
+        );
+    }
+}
+
+AppRegistry.registerComponent('Unagi', () => Unagi);
---
+++ reducers/Reducers.js
@@ -0,0 +1,3 @@
+export const testReducer = (state, action) => {
+    return state;
+}
