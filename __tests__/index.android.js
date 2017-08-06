81b0c94a566ee349c0886002d495cc7341af305c
fatemenajafi <fatemenajafi135@gmail.com>
2017-08-06 19:11:17 +0430

    reducers test

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -1,47 +1,155 @@
 import { mainReducer } from '../reducers/Reducers.js';
 import actions from '../reducers/Actions.js';
-describe("salam", () => {
-  it('should be answered', () => {
 
-  })
-})
-const initialState =
-  {
-    userID: 'userID',
-    location: {x:30,y:30},
-    posts: [{
-      // _id
+const initialState ={
+  userID: 'userID',
+  location: {x:30,y:30},
+  posts: [
+    {
+      _id:1,
       username:'user',
       location:{
         x:37,
-        y=40,
-      },
-      date:new Date(),
-      timeOfLastHotScoreUpdate:new Date(),
-      hotScore:6,
-      like:5,
-      content:'post Content',
-      
-    }],
-    hotPosts: hotPostsReducer,
-    pageName: 'pageNameReducer',
-    userInfo: registerReducer,
-  };
+        y:40,
+    },
+    date:new Date("2017-08-06T13:05:33.340Z"),
+    timeOfLastHotScoreUpdate:new Date("2017-08-06T13:05:33.340Z"),
+    hotScore:6,
+    like:5,
+    content:'post Content',
+    }
+  ],
+  hotPosts: [{
+    _id:2,
+    username:'user2',
+    location:{
+      x:38,
+      y:40,
+    },
+    date:new Date("2017-08-06T13:05:33.340Z"),
+    timeOfLastHotScoreUpdate:new Date("2017-08-06T13:05:33.340Z"),
+    hotScore:6,
+    like:5,
+    content:'post Content 2',
+  }],
+  pageName: 'pageNameReducer',
+  userInfo: {
+    username:'name',
+    password:'pass',
+    accessToken:'accessToken',
+  },
+};
+
 const actionLoction={
-  type: actions.SET_LOCATION,
-  location:{
-    x:0,
-    y:0,
-  }
+    type: actions.SET_LOCATION,
+    location:{
+      x:37,
+      y:40,
+    }
 }
 describe('reducers',()=>{
   describe('locationReducer',()=>{
     it('should be location',()=>{
       expect(mainReducer(initialState,actionLoction).location).toEqual({
-        x:0,
-        y:0
+        x:37,
+        y:40,
       });
     })
+  })
+});
+
+const newPostAction={
+  type: actions.ADD_POSTS,
+  newPosts:[{
+      _id:2,
+      username:'user2',
+      location:{
+        x:38,
+        y:40,
+      },
+      date:new Date("2017-08-06T13:05:33.340Z"),
+      timeOfLastHotScoreUpdate:new Date("2017-08-06T13:05:33.340Z"),
+      hotScore:6,
+      like:5,
+      content:'post Content 2',
+    }],
+}
+
+const likePostAction={
+  type: actions.LIKE,
+  postID:1,
+  // post:{
+    // like:5,
+  // }
+  // post:{
+  //     _id:1,
+  //     username:'user',
+  //     location:{
+  //       x:37,
+  //       y:40,
+  //   },
+  //   date:new Date("2017-08-06T13:05:33.340Z"),
+  //   timeOfLastHotScoreUpdate:new Date("2017-08-06T13:05:33.340Z"),
+  //   hotScore:6,
+  //   like:5,
+  //   content:'post Content',
+  //   }
+}
+
+describe('reducers',()=>{
+  describe('postsReducer',()=>{
+
+    describe('new post',()=>{
+      it('should add post',()=>{
+        expect(mainReducer(initialState,newPostAction).posts).toEqual([{
+          _id:1,
+          username:'user',
+          location:{
+            x:37,
+            y:40,
+          },
+          date:new Date("2017-08-06T13:05:33.340Z"),
+          timeOfLastHotScoreUpdate:new Date("2017-08-06T13:05:33.340Z"),
+          hotScore:6,
+          like:5,
+          content:'post Content',
+          },{
+            _id:2,
+          username:'user2',
+          location:{
+            x:38,
+            y:40,
+          },
+          date:new Date("2017-08-06T13:05:33.340Z"),
+          timeOfLastHotScoreUpdate:new Date("2017-08-06T13:05:33.340Z"),
+          hotScore:6,
+          like:5,
+          content:'post Content 2',
+          }]
+        );
+      })
+    })
+
+    describe('like post',()=>{
+      it('should like post',()=>{
+        expect(mainReducer(initialState,likePostAction).posts[0]).toEqual({
+          _id:1,
+          username:'user',
+          location:{
+            x:37,
+            y:40,
+          },
+          date:new Date("2017-08-06T13:05:33.340Z"),
+          timeOfLastHotScoreUpdate:new Date("2017-08-06T13:05:33.340Z"),
+          hotScore:6,
+          like:6,
+          content:'post Content',
+
+        });
+      })
+    })
+    
 
   })
-})
+});
+
