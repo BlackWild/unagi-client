039445e241604c4f7b77a0edcbc99abfbfe0eae1
fatemenajafi <fatemenajafi135@gmail.com>
2017-08-09 16:27:02 +0430

    sendReplyComponent

---
+++ components/SendReplyScreen.js
@@ -0,0 +1,154 @@
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+  BackHandler,
+} from 'react-native';
+
+import async from 'async';
+
+import { styles } from '../styles/SendPostScreenStyles';
+import { HomeScreen } from './HomeScreen';
+import { setIDState, setLocationState } from '../functions/StateSetters';
+import { sendPost } from '../functions/SendPost';
+
+import { NavigationActions } from 'react-navigation';
+import {addBackHandler} from '../functions/BackHandlerAdder';
+
+import { connect } from 'react-redux';
+
+class SendReplyScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    addBackHandler(this);
+
+    this.isSending = false;
+    this.state = {
+      textLenght: 0,
+      isSending: false
+    }
+
+  }
+
+
+  static navigationOptions = ({ navigation }) => {
+    return {
+      header: null,
+    }
+  };
+
+  sendPostToServer = () => {
+      
+    if (this.isSending || this.state.isSending || this.state.isLengthOverLimit) {
+      return null;
+    } else {
+      
+      this.isSending = true;
+      this.setState({
+        isSending: true
+      }, () => {
+        async.parallel([
+          (callback) => {
+            setLocationState(this).then(() => callback()).catch(() => { });
+          }
+        ], (err) => {
+          sendPost(this.props.accessToken, this.props.location, this.state.text, this).then((res) => {
+            if (res === "ok") {
+              // this.setState({
+              //   isSending: false
+              // }, () => {
+              //   this.isSending = false;
+              //   onPre();
+              // });
+              this.backTouchHandler();
+            }
+          }).catch(() => {
+            this.setState({
+              isSending: false
+            }, () => {
+              this.isSending = false;
+            });
+          });
+        })
+
+      });
+
+    }
+  };
+  backTouchHandler = () => {
+    console.log("back clicked -->   " ,this.props.pageName);
+    this.props.dispatch({type: actions.SET_PAGE_NAME, pageName: "Home"});
+    this.props.navigation.goBack();
+  };
+
+  render() {
+
+    return (
+      <View style={{ flex: 1 }}>
+
+        <View style={styles.bar}>
+
+          <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center' }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <TouchableWithoutFeedback onPress={this.backTouchHandler}>
+                <Image source={require('../img/back.png')} style={styles.pic} />
+              </TouchableWithoutFeedback>
+              <Text style={styles.post}>
+                افزودن پست
+              </Text>
+            </View>
+
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Text style={this.state.isLengthOverLimit ? [styles.charRemain, styles.overChar] : [styles.charRemain, { justifyContent: 'center', alignContent: 'center' }]}>
+                {160 - this.state.textLenght}
+              </Text>
+              <TouchableWithoutFeedback onPress={this.state.isSending?()=>{}:this.sendPostToServer}>
+                <Image source={require('../img/send.png')} style={styles.pic} />
+              </TouchableWithoutFeedback>
+            </View>
+
+          </View>
+
+        </View>
+
+        <View style={styles.container}>
+          <View style={{ flex: 1 }}>
+            <TextInput
+              style={styles.textarea}
+              underlineColorAndroid="transparent"
+              multiline={true}
+              placeholder="اونجا که هستی چه خبره!؟"
+              onChangeText={(text) => {
+                leng = text.split('').length;
+                this.setState({
+                  text,
+                  textLenght: leng,
+                  isLengthOverLimit: leng > 160
+                });
+              }}
+            />
+          </View>
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    accessToken: state.userInfo.accessToken,
+    refreshToken: state.userInfo.refreshToken,
+    username: state.userInfo.username,
+    location: state.location,
+    pageName: state.pageName,
+  }
+};
+
+export default connect(mapStateToProps)(SendReplyScreen);
