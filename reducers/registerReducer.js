9edff4ecb4ef14fd8b233eb3b1b18d07bbc61f90
Siavash <siavash.davani@gmail.com>
2017-08-12 18:26:37 +0430

    dont remove other fields on SET_TOKEN

--- reducers/registerReducer.js
+++ reducers/registerReducer.js
@@ -1,22 +1,22 @@
-import actions from './Actions'
+import actions from "./Actions";
 export const registerReducer = (state = {}, action) => {
-    switch (action.type){
-        case actions.SET_INFO:
-            return {
-                username: action.username,
-                password: action.password,
-                accessToken: action.accessToken,
-                refreshToken: action.refreshToken,
-            };
-            break;
-        case actions.SET_TOKEN:
-            return {
-                accessToken: action.accessToken,
-                refreshToken: action.refreshToken,
-            };
-            break;
-        default:
-            return state;
-
-    }
-}
+  switch (action.type) {
+    case actions.SET_INFO:
+      return {
+        username: action.username,
+        password: action.password,
+        accessToken: action.accessToken,
+        refreshToken: action.refreshToken
+      };
+      break;
+    case actions.SET_TOKEN:
+      return {
+        ...state,
+        accessToken: action.accessToken,
+        refreshToken: action.refreshToken
+      };
+      break;
+    default:
+      return state;
+  }
+};
