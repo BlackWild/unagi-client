4fb67bae1f904b6e37aeaf0752126aca21c744e2
fatemenajafi <fatemenajafi135@gmail.com>
2017-08-12 14:06:12 +0430

    userPage design

---
+++ styles/UserScreenStyles.js
@@ -0,0 +1,56 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  pic: {
+    width: 30,
+    height: 30,
+    margin: 10
+  },
+  header: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center"
+  },
+  bar: {
+    padding: 10,
+    margin: 0,
+    flexDirection: "row",
+    backgroundColor: "#8BC34A",
+    justifyContent: "center"
+  },
+
+  post: {
+    fontSize: 20,
+    color: "#F1F1F1",
+    textAlign: "center",
+    justifyContent: "center",
+    fontFamily: "Vazir"
+  },
+  userBox: {
+    backgroundColor: "#8BC34A",
+    padding: 10,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center"
+  },
+  photo: {
+    // height: 80,
+    // width: 70,
+    // borderRadius: 40,
+    height: 96,
+    width: 84,
+    borderRadius: 48,
+    backgroundColor: "#689F38",
+    borderBottomWidth: 2,
+    borderColor: "#9e9e9e",
+    margin: 10
+  },
+  username: {
+    color: "#ffffff",
+    margin: 10,
+    marginTop: 5,
+    fontSize: 18
+  }
+});
