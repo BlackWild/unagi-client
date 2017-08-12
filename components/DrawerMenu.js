30641437d5472684e765f7fddf442247ec2b668c
Siavash <siavash.davani@gmail.com>
2017-08-12 13:21:51 +0430

    logout function in drawer menu

--- components/DrawerMenu.js
+++ components/DrawerMenu.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Text, View, TouchableWithoutFeedback } from "react-native";
 import { styles } from "../styles/DrawerMenuStyles";
 
-import { connect } from 'react-redux';
-import actions from '../reducers/Actions'
+import { connect } from "react-redux";
+import actions from "../reducers/Actions";
 
 class DrawerMenu extends Component {
-  
-  navig = (pageName) => {
+  navig = pageName => {
+    this.props.dispatch({ type: actions.SET_PAGE_NAME, pageName: pageName });
     this.props.navigation.navigate(pageName);
     this.props.app.closeDrawer();
-  }
+  };
+  logout = () => {
+    this.props.dispatch({
+      type: actions.SET_INFO,
+      username: "",
+      password: "",
+      accessToken: "",
+      refreshToken: ""
+    });
+    this.navig("LogIn");
+  };
 
   render() {
     return (
@@ -20,7 +30,7 @@ class DrawerMenu extends Component {
           <Text style={styles.username}>Username!</Text>
         </View>
         <View>
-          <TouchableWithoutFeedback onPress={()=> this.navig("UserPage")}>
+          <TouchableWithoutFeedback onPress={() => this.navig("UserPage")}>
             <View style={styles.buttonView}>
               <Text style={styles.button}>حساب کاربری</Text>
             </View>
@@ -35,7 +45,7 @@ class DrawerMenu extends Component {
               <Text style={styles.button}>تنظیمات</Text>
             </View>
           </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => this.logout()}>
             <View style={styles.buttonView}>
               <Text style={styles.button}>خروج</Text>
             </View>
@@ -46,11 +56,11 @@ class DrawerMenu extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     app: state.app,
-    navigation: state.navigation,
-  }
+    navigation: state.navigation
+  };
 };
 
 export default connect(mapStateToProps)(DrawerMenu);
